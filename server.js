0456d5973c7e5efcf03b2ba215c27847f346fe77
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-07-23 13:30:53 +0300

    refactor: update server entry point to delegate to router

--- server.js
+++ server.js
@@ -1,69 +1,8 @@
 const { createServer } = require('http');
-const fs = require("fs");
-const validateUserInput = require("./validation");
-const handleUpload = require("./upload");
+const router = require('./routes');
 
 const server = createServer((req, res) => {
-
-    if (req.method === "POST" && req.url === "/upload") {
-        handleUpload(req, res);
-        return;
-    }
-
-    if (req.method === "POST") {
-
-        let body = "";
-
-        req.on("data", chunk => {
-            body += chunk.toString();
-        });
-
-        req.on("end", () => {
-            try {
-                const userData = JSON.parse(body);
-                const users = JSON.parse(fs.readFileSync("users.json", "utf8"));
-
-                const validationResult = validateUserInput(userData, users);
-
-                if (!validationResult.success) {
-                    res.writeHead(400, {
-                        "Content-Type": "application/json"
-                    });
-                    res.end(JSON.stringify({
-                        message: validationResult.error
-                    }));
-                    return;
-                }
-
-                const newUser = {
-                    id: users.length + 1,
-                    ...userData,
-                    createdAt: new Date().toISOString()
-                };
-                users.push(newUser);
-                fs.writeFileSync(
-                    "users.json",
-                    JSON.stringify(users, null, 2)
-                );
-                res.writeHead(201, {
-                    "Content-Type": "application/json"
-                });
-                res.end(JSON.stringify({
-                    message: "User created successfully",
-                    user: newUser
-                }));
-
-            } catch (error) {
-
-                res.writeHead(400, {
-                    "Content-Type": "application/json"
-                });
-                res.end(JSON.stringify({
-                    message: "Invalid JSON"
-                }));
-            }
-        });
-    }
+    router(req, res);
 });
 
 server.listen(3000, () => {
